f1aec4bd1d516bc4ff5dcc82001b0ef40cf075bb
z6daB <patsaifedor08@yandex.ru>
2024-02-03 22:15:48 +0300

    leetcode

--- Add Two Numbers/main.py
+++ Add Two Numbers/main.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         l1 = l1[::-1]
