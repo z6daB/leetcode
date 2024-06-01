97f4fa23f3b04578582cde1518c9231da5f18bc4
z6daB <patsaifedor08@yandex.ru>
2024-06-01 14:41:49 +0300

    leetcode

---
+++ Score of a String/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        summ = 0
+        for i in range(len(s) - 1):
+            summ += abs(ord(s[i]) - ord(s[i + 1]))
+        return summ
+
+sol = Solution()
+print(sol.scoreOfString(s="zaz"))
