90c5691c33644b76c9cc053fc53e5734524476f2
z6daB <patsaifedor08@yandex.ru>
2025-04-20 21:45:13 +0300

    leetcode

--- Two Sum /main.py
+++ Two Sum /main.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return(list((i, j)))
-
+            temp_set = nums[i + 1:]
+            if target - nums[i] in temp_set:
+                return i, nums[i + 1:].index(target - nums[i]) + i + 1
 sol = Solution()
-print(sol.twoSum([1, 2, 3], 4))
+print(sol.twoSum(nums = [2,7,11,15], target = 9))
